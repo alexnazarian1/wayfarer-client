b07ab89857fb8301edaa8b516f78c50047c7ecdd
elissa <elissaeng@Elissas-MacBook-Pro.local>
2021-06-12 18:59:26 -0700

    trying to get edit Post to work

--- src/config/routes.js
+++ src/config/routes.js
@@ -5,6 +5,7 @@ import CitiesIndex from '../pages/CitiesIndex';
 // import CreatePost from '../pages/CreatePost';
 import Login from '../pages/Login';
 import PostShow from '../pages/PostShow'
+import EditPost from '../pages/EditPost'
 
 function Routes(props) {
   return (
@@ -14,6 +15,7 @@ function Routes(props) {
       <Route path='/cities' component={CitiesIndex} />
       {/* <Route path='/createpost' component={CreatePost} /> */}
       <Route path='/post/:id' component={PostShow} />
+      <Route path='/post/:id/edit' component={EditPost} />
     </Switch>
   );
 }
