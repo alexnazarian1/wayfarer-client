e63cc12cf7429b1fb69d6421d739674d21363a49
austinschroeder <austinsoundy@gmail.com>
2021-06-13 12:34:24 -0700

    Added home page content

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
 import CitiesIndex from '../pages/CitiesIndex';
-import PostShow from '../pages/PostShow'
-import EditPost from '../pages/EditPost'
+import PostShow from '../pages/PostShow';
+import EditPost from '../pages/EditPost';
 
 function Routes(props) {
   return (
     <Switch>
-      {/* <Route exact path='/' component={Home} /> */}
+      <Route exact path='/' component={Home} />
       {/* <Route path='/login' component={Login} /> */}
       <Route path='/cities' component={CitiesIndex} />
       {/* <Route path='/createpost' component={CreatePost} /> */}
